5f685bd48ea2bc3c0e29ea7375197611a683a803
Jesse Wenger <wengerj@gmail.com>
2021-03-20 00:18:16 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 1, "M", "honey", "sleepy", 0),
+(2, "Rowdy", 2, "M", "polar", "grumpy", 0),
+(3, "Tabitha", 3, "F", "black", "silly", 1),
+(4, "Sargeant Brown", 4, "M", "honey", "mean", 0),
+(5, "Melissa", 5, "F", "brown", "harsh", 0),
+(6, "Grinch", 6, "M", "honey", "sweet", 1),
+(7, "Wendy", 7, "F", "honey", "bumbly", 1),
+(8, null, 8, "M", "black", "bully", 1);
